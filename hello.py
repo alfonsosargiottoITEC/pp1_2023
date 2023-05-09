12ea9d02f32c2ffc3fb4b499f4cb7b6ecaa50a1f
Alfonso Sargiotto <a.sargiotto@itecriocuarto.org.ar>
2023-05-09 07:35:36 -0300

    add printing function

--- hello.py
+++ hello.py
@@ -3,3 +3,14 @@ print("hello World")
 
 for x in range(3):
     print ("hello world for loop")
+    
+    
+phrase = "This is what I want to print"
+number_of_times = 10
+
+def print_text_x_times(text:str, number_of_times:int):
+    for x in range(number_of_times):
+        print (text)
+        
+        
+print_text_x_times(phrase, number_of_times)
